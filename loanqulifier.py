c248817361cd89dc00d47d8f2f3d877479743f25
cxy7988 <cxy7988@gmail.com>
2024-02-29 16:22:47 +0800

    developer

---
+++ loanqulifier.py
@@ -0,0 +1,27 @@
+#loanqulifierapp.py
+#This application is for test the qualification of loan
+
+import streamlit as st
+st.title("Brian's loanqulifier")
+#set minimum standard
+MINSALARY = 30000
+MINYEAR = 5
+
+#Get input
+salary = float(st.number_input("please enter your salary:"))
+year = int(st.number_input("please enter your year:"))
+
+#Determine qualification
+if st.button("Submit"):
+    if salary > MINSALARY and year >  MINYEAR:
+        st.write("Your application is accepted")
+    else:
+        st.write("Your application is rejected")
+else:
+    st.write("Please enter your data")
+
+import streamlit as st
+import numpy as np
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
